43c0ed23577b318b02d26ff7e14d2d45c7c23c2d
Islahihyaa <islah.ihya@gmail.com>
2024-05-12 17:14:17 +0700

    RPL450102-157 Create fitur manage vehicle

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\schedulepickupController;
 use App\Http\Middleware\AuthenticatingMiddleware;
 
+
+//* Authentication
 Route::middleware(AuthenticatingMiddleware::class)->group(function () {
     Route::get('/', [AuthController::class, 'login']);
     Route::post('/', [AuthController::class, 'authenticating']);
@@ -19,10 +21,43 @@ Route::get('logout', [AuthController::class, 'logout']);
 Route::get('home', [UserController::class, 'getHome']);
 Route::get('dashboard', [AdminController::class, 'getDashboard']);
 
+//* End Authentication
+
+//* Penjemputan Sampah
 Route::get('order', [UserController::class, 'createOrder']);
 Route::post('order', [UserController::class, 'submitOrder'])->name('order');
+//* End Penjemputan Sampah
 
+//* History
 Route::get('history', [UserController::class, 'getHistory']);
 Route::get('history', [schedulepickupController::class, 'getHistory']);
 
+//* End History
+
+//* Redeems Points
 Route::get('redeemspoints', [UserController::class, 'getRedeemspoints']);
+
+//* End Redeems Points
+
+//* Customer Sercice 
+Route::get('customer-service', [UserController::class, 'getCustomerService']);
+Route::post('customer-service', [UserController::class, 'submitComplaint']);
+Route::put('customer-service/{complaint_id}', [AdminController::class, 'updateStatus'])->name('customer-service.update');
+Route::get('complaint-delete/{complaint_id}', [AdminController::class, 'deleteComplaint']);
+
+//* End Customer Sercice 
+
+
+/*--------------------------------------------------------------
+# Admin
+--------------------------------------------------------------*/
+
+Route::get('response-complaint', [AdminController::class, 'getResponseComplaint']);
+
+//* Kelola Kendaraan
+Route::get('manage-vehicles', [AdminController::class, 'getManageVehicle']);
+Route::put('manage-vehicles/{vehicle_id}', [AdminController::class, 'updateStatusVehicle'])->name('status-vehicle.update');
+Route::get('add-vehicles', [AdminController::class, 'getAddVehicle']);
+Route::post('add-vehicles', [AdminController::class, 'submitAddVehicle']);
+Route::get('manage-vehicles/{vehicle_id}', [AdminController::class, 'deleteVehicle'])->name('data-vehicle.delete');
+//* End Kelola Kendaraan
