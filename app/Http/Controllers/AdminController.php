538e2664eb821c30bc46af836bb4e992e340fc1e
ddavidfadila <115241668+ddavidfadila@users.noreply.github.com>
2024-05-16 10:09:55 +0700

    david

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Complaint;
+use Session;
 
 class AdminController extends Controller
 {
     public function getDashboard()
     {
         return view('admin.dashboard');
+    }
+
+    public function getResponseComplaint()
+    {
+        $complaintdata = Complaint::all();
+        return view('admin.response-complaint', compact('complaintdata'));
+    }
+
+    public function deleteComplaint($complaint_id)
+    {
+        $deleteComplaint = Complaint::find($complaint_id);
+
+        $deleteComplaint->delete();
+        
+        if($deleteComplaint) {
+            Session::flash('deleteComplaint','Data Deleted Succesfully');
+            return redirect('response-complaint');
+        } else {
+            dd($deleteComplaint);
+        }
+
+    }
+
+    public function updateStatus(Request $request, $complaint_id)
+    {
+        $statusComplaint = Complaint::find($complaint_id);
+        $statusComplaint->status = $request->input('status');
+        $statusComplaint->save();
+
+        Session::flash('updateStatus',"Data Updated To $statusComplaint->status");
+        return redirect('response-complaint');
+
     }
 }
