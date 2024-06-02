f4bed0b928953e579590535430794e88c0b5402c
Tantriip <tantripalwita@gmail.com>
2024-06-02 17:59:09 +0700

    Article1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,7 +24,7 @@ class AdminController extends Controller
         $deleteComplaint = Complaint::find($complaint_id);
 
         $deleteComplaint->delete();
-        
+
         if($deleteComplaint) {
             Session::flash('deleteComplaint','Data Deleted Succesfully');
             return redirect('response-complaint');
@@ -43,5 +43,76 @@ class AdminController extends Controller
         Session::flash('updateStatus',"Data Updated To $statusComplaint->status");
         return redirect('response-complaint');
 
+    }
+
+    public function getArticle()
+    {
+        $data_article = Article::all();
+        return view('admin.manage-article', compact('data_article'));
+    }
+
+    public function show_detail_article(Request $request) {
+        $article = Article::find($request->article_id);
+
+        return view('admin.detail-article', [
+            'article' => $article
+        ]);
+    }
+
+    public function getAddArticle()
+    {
+        return view('admin.add-article');
+    }
+
+    public function submitAddArticle(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'image_article' => 'required',
+        ]);
+
+        $article_image_path = $request->file('image_article')->store('article-image', 'public');
+
+        $createAddArticle = Article::create([
+            'title' => $request->input('title'),
+            'content'=> $request->input('content'),
+            'image_article' => $article_image_path,
+        ]);
+
+        if($createAddArticle) {
+            Session::flash('status','Artikel Berhasil di Publish'); // Perubahan
+            return redirect('add-article');
+        } else {
+            Session::flash('notSetDataMessage', 'Data Article Gagal Ditambahkan');
+            return redirect('add-article');
+        }
+    }
+
+    public function destroy_article(Request $request) {
+        $article = Article::find($request->article_id);
+
+        $article->delete();
+
+        Session::flash('success-to-delete-article', 'Data Arttkel' . $article->title . ' Berhasil Dihapus');
+        return redirect(url('manage-article'));
+    }
+
+    public function editArticle(Request $request)
+    {
+        $article = Article::find($request->article_id);
+
+        return view('admin.update-article', compact('article'));
+    }
+    public function updateartikel(Request $request)
+    {
+        $article = Article::find($request->article_id);
+        $article->title = $request->input('title');
+        $article->content = $request->input('content');
+        $article->image_article = $request->input('image_article');
+
+
+        $item->save();
+        return redirect('articleupp/')->with('success', 'Data updated successfully');
     }
 }
