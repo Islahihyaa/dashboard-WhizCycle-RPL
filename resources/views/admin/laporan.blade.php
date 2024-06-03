4fb93d2d0c449636ec9f5cc3aac21132798f5d40
alfidaulay123 <alfidaulay@student.telkomuniversity.ac.id>
2024-06-03 13:37:24 +0700

    final alfidaulay laporan

--- resources/views/admin/laporan.blade.php
+++ resources/views/admin/laporan.blade.php
@@ -16,19 +16,23 @@
                     <div class="card-body">
                         <h5 class="card-title">Cetak Laporan</h5>
                         <div class="row">
-                            <div class="col-md-3 col-sm-6 mb-3">
+                            <!-- Top row with 3 buttons -->
+                            <div class="col-md-4 col-sm-6 mb-3">
                                 <a href="{{ url('/data-point') }}" class="btn-custom d-block"><i class="bi bi-plus-circle"></i><span class="m-2">Data Tukar Point</span></a>
                             </div>
-                            <div class="col-md-3 col-sm-6 mb-3">
+                            <div class="col-md-4 col-sm-6 mb-3">
                                 <a href="{{ url('/data-cs') }}" class="btn-custom d-block"><i class="bi bi-plus-circle"></i><span class="m-2">Data Customer Service</span></a>
                             </div>
-                            <div class="col-md-3 col-sm-6 mb-3">
+                            <div class="col-md-4 col-sm-6 mb-3">
                                 <a href="{{ url('/data-user') }}" class="btn-custom d-block"><i class="bi bi-plus-circle"></i><span class="m-2">Data User</span></a>
                             </div>
-                            <div class="col-md-3 col-sm-6 mb-3">
+                        </div>
+                        <div class="row">
+                            <!-- Bottom row with 2 buttons -->
+                            <div class="col-md-6 col-sm-6 mb-3">
                                 <a href="{{ url('/data-vehicles') }}" class="btn-custom d-block"><i class="bi bi-plus-circle"></i><span class="m-2">Data Kendaraan</span></a>
                             </div>
-                            <div class="col-md-3 col-sm-6 mb-3">
+                            <div class="col-md-6 col-sm-6 mb-3">
                                 <a href="{{ url('/data-driver') }}" class="btn-custom d-block"><i class="bi bi-plus-circle"></i><span class="m-2">Data Driver</span></a>
                             </div>
                         </div>
